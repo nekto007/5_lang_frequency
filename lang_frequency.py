e8698f691ac3da6dcbfdfac486e032982af59dca
Igor <nekto.korobko@gmail.com>
2017-04-26 00:49:55 +0300

    Улучшен скрипт обработки файла и обновлено описание

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,25 +1,21 @@
 import sys
+import collections
 def load_data(filepath):
     with open(filepath, 'r') as listwords:
-        text = listwords.read()
+        text = listwords.read().split()
     return text
 
 
 def get_most_frequent_words(text):
-    listwords = text.split()
-    dictcountwords = {}
     topwords = []
-    for i in set(listwords):
+    for i in text:
         if i.isalpha():
-            dictcountwords[i] = listwords.count(i)
-    for k,v in sorted(dictcountwords.items(), key=lambda x: x[1], reverse=True):
-        topwords.append(k)
-        if len(topwords) > 10:
-            break
+            topwords.append(i)
+    topwords = collections.Counter(topwords).most_common(10)
     return topwords
 
 if __name__ == '__main__':
     filepath = (sys.argv[1])
     text = load_data(filepath)
     topwords = get_most_frequent_words(text)
-    print('10 самых популярных слов: ', topwords)
+    print(('%s самых популярных слов: ' % (len(topwords))), topwords)
