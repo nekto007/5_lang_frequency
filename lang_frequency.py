c9fb67ee7d75463576cb7e1d4167477adc504df1
Igor <nekto.korobko@gmail.com>
2017-04-26 21:40:58 +0300

    Магиские числа вынесены в переменную и добавлен обработчик текста

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,18 +1,26 @@
 import sys
+import re
 import collections
 def load_data(filepath):
     with open(filepath, 'r') as listwords:
-        text = listwords.read().split()
+        text = listwords.read()
+        text = get_clean_string(text)
     return text
 
 
 def get_most_frequent_words(text):
+    topnumber= 10
     topwords = []
     for i in text:
         if i.isalpha():
             topwords.append(i)
-    topwords = collections.Counter(topwords).most_common(10)
+    topwords = collections.Counter(topwords).most_common(topnumber)
     return topwords
+
+
+def get_clean_string(text):
+    text = re.sub(r'[^\w\s]', '', ' '.join(text.split())).lower().split(' ')
+    return text
 
 if __name__ == '__main__':
     filepath = (sys.argv[1])
